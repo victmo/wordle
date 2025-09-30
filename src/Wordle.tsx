f4b2496864e230ddad29b390185abb28f8c937c6
Victor Marquez <victmo@gmail.com>
2025-09-30 14:48:23 -0400

    Add  game logic (minus keyboard highlights)

--- src/Wordle.tsx
+++ src/Wordle.tsx
@@ -1,6 +1,6 @@
 import './Wordle.css';
 
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const keyboardRows = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'];
 const alphabet = keyboardRows.join('');
@@ -19,19 +19,69 @@ export type WordleProps = {
 export const Wordle = (props: WordleProps) => {
   const { word, length, attempts } = props;
   const [guesses, setGuesses] = useState<string[]>([]);
+  const [currentValue, setCurrentValue] = useState('');
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isWin, setIsWin] = useState(false);
+
+  // Handle (virtual and physical) key presses
+  const handleKeyPress = useCallback(
+    (key: string) => {
+      const letter = key.toLowerCase();
+
+      if (key === 'Enter') {
+        if (currentValue.length !== length) {
+          return;
+        }
+        setGuesses([...guesses, currentValue]);
+        setCurrentValue('');
+      } else if (key === 'Backspace') {
+        setCurrentValue(currentValue.slice(0, -1));
+      } else if (alphabet.includes(letter) && currentValue.length < length) {
+        setCurrentValue(currentValue + letter);
+      }
+    },
+    [currentValue, guesses, length]
+  );
+
+  // Sync game state
+  useEffect(() => {
+    const won = guesses.includes(word);
+    setIsWin(won);
+
+    if (guesses.length >= attempts || won) {
+      setIsGameOver(true);
+    }
+  }, [guesses, word, attempts]);
+
+  // Add keyboard listener
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (!isGameOver) {
+        handleKeyPress(e.key);
+      }
+    };
+    window.addEventListener('keydown', listener);
+    return () => window.removeEventListener('keydown', listener);
+  }, [isGameOver, currentValue, guesses, handleKeyPress]);
 
   return (
     <div>
       <h2>Hint: {word}</h2>
       <div className="board">
-        <Line value="beast" complete length={length} word={word} />
-        <Line value="bemak" complete length={length} word={word} />
-        <Line value="asd" length={length} word={word} />
-        <Line length={length} word={word} />
+        {Array.from({ length: attempts }).map((_, i) => (
+          <Line
+            key={i}
+            length={length}
+            word={word}
+            complete={i < guesses.length}
+            value={i === guesses.length ? currentValue : guesses[i]}
+          />
+        ))}
       </div>
 
       <Keyboard
         rows={keyboardRows}
+        onKeyPress={isGameOver ? undefined : handleKeyPress}
         guessedLetters={
           new Map([
             ['a', 'misplaced'],
@@ -88,10 +138,11 @@ export const Tile = (props: TileProps) => {
 type KeyboardProps = {
   rows: string[];
   guessedLetters: Map<string, LetterStatus>;
+  onKeyPress?: (key: string) => void;
 };
 
 export const Keyboard = (props: KeyboardProps) => {
-  const { rows, guessedLetters } = props;
+  const { rows, guessedLetters, onKeyPress } = props;
   return (
     <div className="keyboard">
       {rows.map((row, i) => (
@@ -99,11 +150,35 @@ export const Keyboard = (props: KeyboardProps) => {
           {row.split('').map(letter => {
             const status = guessedLetters.get(letter) || 'empty';
             return (
-              <button key={letter} className={`key ${status}`}>
+              <button
+                key={letter}
+                className={`key ${status}`}
+                type="button"
+                onClick={() => onKeyPress?.(letter)}
+              >
                 {letter}
               </button>
             );
           })}
+          {i === rows.length - 1 && (
+            <>
+              <button
+                type="button"
+                className="key empty enter"
+                onClick={() => onKeyPress?.('Enter')}
+              >
+                Enter
+              </button>
+
+              <button
+                type="button"
+                className="key empty backspace"
+                onClick={() => onKeyPress?.('Backspace')}
+              >
+                ⌫
+              </button>
+            </>
+          )}
         </div>
       ))}
     </div>
